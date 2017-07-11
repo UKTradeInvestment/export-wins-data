35e963ce1ca0766b62eaf8447406ab6a2911cf60
sekharpanja <sekharpanja@gmail.com>
2017-07-11 11:07:46 +0100

    WIP

--- data/middleware.py
+++ data/middleware.py
@@ -1,16 +1,10 @@
-from django.http import HttpResponseForbidden, HttpResponsePermanentRedirect
+from django.conf import settings
+from django.http import HttpResponseBadRequest
 from django.middleware.security import SecurityMiddleware
 
 
 class HttpsSecurityMiddleware(SecurityMiddleware):
     def process_request(self, request):
-        if not request.is_secure:
-            return HttpResponseForbidden()
-        path = request.path.lstrip("/")
-        if (self.redirect and not request.is_secure() and
-                not any(pattern.search(path)
-                        for pattern in self.redirect_exempt)):
-            host = self.redirect_host or request.get_host()
-            return HttpResponsePermanentRedirect(
-                "https://%s%s" % (host, request.get_full_path())
-            )
+        """ Enforce https for all requests except when Debugging """
+        if not settings.DEBUG and not request.is_secure():
+            return HttpResponseBadRequest()
