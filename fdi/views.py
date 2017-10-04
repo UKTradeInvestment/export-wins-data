fa8d89822fdad1d042c09afea3760b414b4d286e
Ravi Kotecha <kotecha.ravi@gmail.com>
2017-10-04 17:48:08 +0100

    Implement FDI overview
    This leverages postgres features to provide a very quick overview page,
    we can, in the future, take these ideas back to Export Wins

--- fdi/views.py
+++ fdi/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Func, F, Sum, Count
+
 from fdi.models import Investments
 from core.views import BaseMIView
 from mi.models import Sector
@@ -9,6 +11,12 @@ class BaseFDIView(BaseMIView):
 
     def get_queryset(self):
         return self.queryset
+
+    def get_results(self):
+        return []
+
+    def get(self, request):
+        return self._success(self.get_results())
 
 
 class FDISectorTeamListView(BaseFDIView):
@@ -23,9 +31,25 @@ class FDISectorOverview(BaseFDIView):
 
 
 class FDIOverview(BaseFDIView):
+    pass
+
+
+class FDIYearOnYearComparison(BaseFDIView):
 
     def get_results(self):
-        return []
-
-    def get(self, request):
-        return self._success(self.get_results())
+        return self.get_queryset().won().values(
+            'date_won'
+        ).annotate(
+            year=Func(F('date_won'), function='get_financial_year')
+        ).annotate(
+            Count('year'),
+            Sum('number_new_jobs'),
+            Sum('number_safeguarded_jobs'),
+            Sum('investment_value')
+        ).values(
+            'year',
+            'year__count',
+            'number_new_jobs__sum',
+            'number_safeguarded_jobs__sum',
+            'investment_value__sum'
+        ).order_by('year')
