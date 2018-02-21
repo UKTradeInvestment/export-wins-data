09c2cfa9259d079a181731acbc422237408c0517
Ravi Kotecha <kotecha.ravi+github@gmail.com>
2018-02-21 18:33:19 +0000

    Fdi/test overview scenarios (#391)
    Using django subtests to check all scenarios for a single non_hvc win.
    
    Checks for all won stages and all FDIValues
    
    also introduces pytest to run tests incrementally

--- fdi/tests/test_performance.py
+++ fdi/tests/test_performance.py
@@ -1,58 +1,159 @@
-from django.urls import reverse
-from toolz import get_in
+from uuid import UUID
+from copy import deepcopy
 
+from django.urls import reverse
+from factory.fuzzy import FuzzyDate
+
+from fdi.models import Investments, FinancialYear
 from fdi.tests.base import FdiBaseTestCase
+from fdi.factories import InvestmentFactory
+
+
+FDIValueMapping = {
+    UUID('38e36c77-61ad-4186-a7a8-ac6a1a1104c6'): 'high',
+    UUID('002c18d9-f5c7-4f3c-b061-aee09fce8416'): 'good',
+    UUID('2bacde8d-128f-4d0a-849b-645ceafe4cf9'): 'standard'
+}
+
+STAGES = ['won', 'prospect', 'active', 'assign_pm', 'verify_win']
 
 class FDIOverviewTestCase(FdiBaseTestCase):
+    maxDiff = None
     url = reverse('fdi:overview')
+    factory = InvestmentFactory
+
+    @property
+    def empty_response(self):
+        zero_count_and_percent = {
+            "count": 0,
+            "percent": 0.0
+        }
+
+        jobs = {
+            "new": 0,
+            "safeguarded": 0,
+            "total": 0
+        }
+
+        stages = {k: deepcopy(zero_count_and_percent) for k in STAGES}
+        campaign = {k: deepcopy(zero_count_and_percent) for k in ['hvc', 'non_hvc']}
+
+        performance_section = {
+            **zero_count_and_percent,
+            'campaign': deepcopy(campaign),
+            'jobs': deepcopy(jobs),
+            'on_target': False
+        }
+
+        performance = {k: deepcopy(performance_section) for k in FDIValueMapping.values()}
+
+
+        return deepcopy({
+            "wins": {
+                "count": 0,
+                "total_investment_value__sum": 0,
+                "jobs": deepcopy(jobs),
+                "campaign": deepcopy(campaign),
+                "performance": deepcopy(performance),
+                "stages": deepcopy(stages)
+            },
+            "pipeline": {
+                "active": {
+                    "count": 0,
+                    "total_investment_value__sum": 0,
+                    "jobs": deepcopy(jobs),
+                    "campaign": deepcopy(campaign),
+                    "performance": deepcopy(performance),
+                    "stages": deepcopy(stages)
+                }
+            },
+            "stages": deepcopy(stages)
+        })
 
     def assert_response_zeros(self, api_response):
-        wins_base_path = ['wins']
-        pipeline_base_path = ['pipeline']
-        pipeline_active_path = pipeline_base_path + ['active']
-        paths = [
-            wins_base_path + ['count'],
-            wins_base_path + ['total_investment_value__sum'],
-            wins_base_path + ['jobs', 'new'],
-            wins_base_path + ['jobs', 'safeguarded'],
-            wins_base_path + ['jobs', 'total'],
-            wins_base_path + ['campaign', 'hvc', 'count'],
-            wins_base_path + ['campaign', 'hvc', 'percent'],
-            wins_base_path + ['campaign', 'non_hvc', 'count'],
-            wins_base_path + ['campaign', 'non_hvc', 'percent'],
-        ]
-        for v in ['high', 'good', 'standard']:
-            for base in [wins_base_path, pipeline_active_path]:
-                performance_base_path = base + ['performance']
-
-                performance_paths = [
-                    performance_base_path + [v, 'count'],
-                    performance_base_path + [v, 'percent'],
-                    performance_base_path + [v, 'campaign', 'hvc', 'count'],
-                    performance_base_path + [v, 'campaign', 'hvc', 'percent'],
-                    performance_base_path + [v, 'campaign', 'non_hvc', 'count'],
-                    performance_base_path + [v, 'campaign', 'non_hvc', 'percent'],
-                    performance_base_path + [v, 'jobs', 'new'],
-                    performance_base_path + [v, 'jobs', 'safeguarded'],
-                    performance_base_path + [v, 'jobs', 'total'],
-                    performance_base_path + [v, 'on_target'],
-                ]
-                paths.extend(performance_paths)
-        for stage in ['won', 'prospect', 'active', 'assign_pm', 'verify_win']:
-            for base in [wins_base_path, pipeline_active_path, []]:
-                stage_paths = [
-                    base + ['stages', stage, 'count'],
-                    base + ['stages', stage, 'percent']
-                ]
-                paths.extend(stage_paths)
-        for path in paths:
-            self.assertEqual(get_in(path, api_response, no_default=True), 0, path)
+        self.assertDictEqual(api_response, self.empty_response)
 
     def test_overview_no_wins_2017(self):
         self.url = self.get_url_for_year(2017, self.url)
         api_response = self._api_response_data
         self.assert_response_zeros(api_response)
 
+    def test_overview_1_win_all_fdi_values_won_and_verify_win(self):
+        fdi_values = FDIValueMapping.keys()
+        stages = ['won', 'verify win']
+        hvcs = [True, False]
+        years = FinancialYear.objects.all()
+        base_url = self.url
+        for year in years:
+            self.url = self.get_url_for_year(year.id, base_url)
+            for value_id in fdi_values:
+                for stage in stages:
+                    for hvc in hvcs:
+                        with self.subTest(value_id=value_id, stage=stage, year=year, hvc=hvc):
+                            self._test_single_win(stage, value_id, year, hvc)
+                        # clean up
+                        Investments.objects.all().delete()
+
+    def _test_single_win(self, stage, value_id, year, hvc):
+        self.assertEqual(Investments.objects.count(), 0)
+        hvc_code = 'I123' if hvc else None
+        win = self.factory(
+            fdi_value_id=value_id,
+            stage=stage,
+            hvc_code=hvc_code,
+            date_won=FuzzyDate(year.start, min(year.end, self.frozen_date_17))
+        )
+        stage = stage.replace(' ', '_')
+
+        self.assertEqual(Investments.objects.count(), 1)
+
+        if stage == 'won':
+            self.assertEqual(Investments.objects.won().count(), 1)
+
+        self.assertEqual(Investments.objects.won_and_verify().count(), 1)
+
+        # make api call
+        api_response = self._api_response_data
+        expected = self.empty_response
+
+        wins = expected['wins']
+        wins['count'] = 1
+        wins['total_investment_value__sum'] = win.investment_value
+
+        jobs = wins['jobs']
+        jobs['new'] = win.number_new_jobs
+        jobs['safeguarded'] = win.number_safeguarded_jobs
+        jobs['total'] = win.number_safeguarded_jobs + win.number_new_jobs
+
+        campaign = wins['campaign'] # type: dict
+        hvc_path = 'hvc' if hvc else 'non_hvc'
+
+        campaign[hvc_path]['count'] = 1
+        campaign[hvc_path]['percent'] = 100.0
+
+        value = FDIValueMapping[win.fdi_value_id]
+        performance = wins['performance'][value] # type: dict
+        performance['count'] = 1
+        performance['percent'] = 100.0
+        performance['campaign'][hvc_path]['count'] = 1
+        performance['campaign'][hvc_path]['percent'] = 100.0
+        performance['jobs']['new'] = win.number_new_jobs
+        performance['jobs']['safeguarded'] = win.number_safeguarded_jobs
+        performance['jobs']['total'] = win.number_safeguarded_jobs + win.number_new_jobs
+        # TODO: handle 'on_target' later
+        expected['wins']['performance']['high']['on_target'] = api_response['wins']['performance']['high']['on_target']
+        expected['wins']['performance']['good']['on_target'] = api_response['wins']['performance']['good']['on_target']
+
+        stage_part = wins['stages'][stage] # type: dict
+        stage_part['count'] = 1
+        stage_part['percent'] = 100.0
+
+        global_stage_part = expected['stages'][stage] # type: dict
+        global_stage_part['count'] = 1
+        global_stage_part['percent'] = 100.0
+
+        self.assertDictEqual(api_response, expected)
+        Investments.objects.all().delete()
 
     def test_overview_no_wins_2016(self):
         self.url = self.get_url_for_year(2016, self.url)
--- fdi/views.py
+++ fdi/views.py
@@ -78,7 +78,7 @@ def fill_in_missing_stages(data):
     return fill_in_missing(
         data,
         ['won', 'prospect', 'active', 'assign pm', 'verify win'],
-        {'count': 0, 'percent': 0},
+        {'count': 0, 'percent': 0.0},
     )
 
 
@@ -125,7 +125,7 @@ def add_is_on_target(data):
     high_and_good_percent = high_percent + data['good']['percent']
     data['high']['on_target'] = high_percent > 40
     data['good']['on_target'] = high_and_good_percent > 70
-    data['standard']['on_target'] = 100 - high_and_good_percent < 30
+    data['standard']['on_target'] = False
     return data
 
 
