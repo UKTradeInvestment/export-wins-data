4c062e7251523737761d5d393e3ce2ce586ae327
Adam L Edwards <adam@aeweb.io>
2020-04-20 12:02:19 +0100

    Add post save signal to win model

---
+++ wins/signals.py
@@ -0,0 +1,26 @@
+import logging
+
+from django.db import transaction
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+from wins.tasks.match_id_task import update_match_id
+from wins.models import Win
+
+logger = logging.getLogger(__name__)
+
+
+@receiver(post_save, sender=Win, dispatch_uid='win_post_save_dispatch')
+def save_match_id(sender, instance, **kwargs):
+    """Save match id on a post save signal."""
+    update_fields = kwargs['update_fields']
+    if update_fields is None:
+        update_fields = []
+    """
+    Ignore fixture update of if the match_id has been passed in
+    update_fields e.g Win.save(update_fields=['match_id'])
+    """
+    if kwargs['raw'] or 'match_id' in update_fields:
+        return
+
+    transaction.on_commit(lambda: update_match_id.delay(instance.pk))
