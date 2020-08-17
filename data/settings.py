130f38bec1860894d19a8449ba6ab2d81d00b5ca
Sekhar Panja <sekharpanja@gmail.com>
2020-08-17 12:42:27 +0100

    change few countries to bring export wins inline with reference data across the department

--- data/settings.py
+++ data/settings.py
@@ -324,6 +324,31 @@ COUNTRIES_OVERRIDE = {
     'XG': 'Global',
     'FK': 'Falkland Islands',
     'CZ': 'Czech Republic',
+    'BQ': 'Bonaire',
+    'BS': 'The Bahamas',
+    'CD': 'Congo (Democratic Republic)',
+    'CI': 'Ivory Coast',
+    'CV': 'Cape Verde',
+    'CZ': 'Czechia',
+    'FM': 'Micronesia',
+    'GM': 'The Gambia',
+    'GS': 'South Georgia and South Sandwich Islands',
+    'KN': 'St Kitts and Nevis',
+    'LC': 'St Lucia',
+    'MF': 'Saint-Martin (French part)',
+    'MK': 'North Macedonia',
+    'MM': 'Myanmar (Burma)',
+    'PN': 'Pitcairn, Henderson, Ducie and Oeno Islands',
+    'PS': 'Occupied Palestinian Territories',
+    'SH': 'Saint Helena',
+    'SZ': 'Eswatini',
+    'TL': 'East Timor',
+    'UM': 'Palmyra Atoll',
+    'US': 'United States',
+    'VA': 'Vatican City',
+    'VC': 'St Vincent',
+    'VG': 'British Virgin Islands',
+    'VI': 'United States Virgin Islands',
 }
 
 # Hawk Authentication
@@ -393,10 +418,10 @@ def get_redis_instance():
     return redis_service['credentials']
 
 
-redis_credentials = get_redis_instance()
+# redis_credentials = get_redis_instance()
 
 
-redis_uri = redis_credentials['uri']
+redis_uri = "redis://127.0.0.1:6380/"
 
 
 def _build_redis_url(base_url, db_number=0, **query_args):
